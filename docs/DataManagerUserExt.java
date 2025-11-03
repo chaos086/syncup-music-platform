3ebeca4714dd3660af16703f7ef84392b6cccd57
Alejandro Marín Hernández <141966025+chaos086@users.noreply.github.com>
2025-11-03 04:44:10 -0500

    chore: add note file for createUser signature (will add method in DataManager)

---
+++ docs/DataManagerUserExt.java
@@ -0,0 +1,8 @@
+package com.syncup.data;
+
+import com.syncup.models.Usuario;
+
+public class DataManagerUserExt {
+    // Este archivo es solo para documentar la firma requerida en RegisterDialogController
+    // createUser(String username, String password, String nombre, String email)
+}
